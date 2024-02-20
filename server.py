13e56badd04f9f13c064166850018245250af6d6
Valik <walik7496@gmail.com>
2024-02-20 22:00:53 +0200

    init

---
+++ server.py
@@ -0,0 +1,41 @@
+# Author: NeuralNine
+import socket
+import threading
+import queue
+
+message_queue = queue.Queue()
+clients = []
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind(("localhost", 9999))
+
+def receive():
+    while True:
+        try:
+            message, addr = server.recvfrom(1024)
+            message_queue.put((message, addr))
+        except:
+            pass
+
+def broadcast():
+    while True:
+        while not message_queue.empty():
+            message, addr = message_queue.get()
+            print(message.decode())
+            if addr not in clients:
+                clients.append(addr)
+            for client in clients:
+                try:
+                    if message.decode().startswith("SIGNUP_TAG:"):
+                        name = message.decode()[message.decode().index(":")+1:]
+                        server.sendto(f"{name} joined!".encode(), client)
+                    else:
+                        server.sendto(message, client)
+                except:
+                    clients.remove(client)
+
+t1 = threading.Thread(target=receive)
+t2 = threading.Thread(target=broadcast)
+
+t1.start()
+t2.start()
